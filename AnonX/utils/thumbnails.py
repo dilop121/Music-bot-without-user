1c94fbaf261db801387f90e6cd52213a2ac66963
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-08-21 13:26:31 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -112,16 +112,16 @@ async def gen_thumb(videoid, user_id):
         logo.thumbnail((200, 200), Image.LANCZOS)
         width = int((1280 - 200) / 25)
         background = Image.open(f"cache/temp{videoid}.png")
-        background.paste(logo, (width + 3, 300), mask=logo)
+        background.paste(logo, (width + 3, 290), mask=logo)
         background.paste(x, (1050, 250), mask=x)
         background.paste(image3, (0, 0), mask=image3)
 
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("AnonX/assets/font2.ttf", 35)
-        ImageFont.truetype("AnonX/assets/font2.ttf", 60)
-        arial = ImageFont.truetype("AnonX/assets/font2.ttf", 25)
-        ImageFont.truetype("AnonX/assets/font.ttf", 25)
-        para = textwrap.wrap(title, width=28)
+        font = ImageFont.truetype("AnonX/assets/font2.ttf", 45)
+        ImageFont.truetype("AnonX/assets/font2.ttf", 70)
+        arial = ImageFont.truetype("AnonX/assets/font2.ttf", 30)
+        ImageFont.truetype("AnonX/assets/font.ttf", 30)
+        para = textwrap.wrap(title, width=32)
         try:
             draw.text(
                 (50, 25),
@@ -134,7 +134,7 @@ async def gen_thumb(videoid, user_id):
             if para[0]:
                 text_w, text_h = draw.textsize(f"{para[0]}", font=font)
                 draw.text(
-                    ((1280 - 1120) / 3.50, 590),
+                    ((1280 - 1115) / 3.50, 595),
                     f"{para[0]} {para[1]}",
                     fill="white",
                     stroke_width=1,
